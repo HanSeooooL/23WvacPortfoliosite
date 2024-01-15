e010a324361e08df2f097b07ec372158ef88a7de
sssseol <nothinonthere@icloud.com>
2024-01-15 04:54:08 +0000

    [update] little

--- pages/Project.js
+++ pages/Project.js
@@ -1,9 +1,17 @@
+import { useEffect, useState } from "react"
+
 export default function Projects() {
+    
+    const [projects, setProjects] = useState([])
+    useEffect(() => {
+        fetch('http://localhost:3003/api/DB/selectProject')
+                .then(function(response) {
+                    response.json()
+                }).then((data) => setProjects(data))
+    }, []) 
+    
     return (
-        <>
-            <div>
-                Projects
-            </div>
-        </>
+        console.log()
+        
     )
 }
